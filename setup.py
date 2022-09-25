5aa981a53777e253f99d36cac6c3cdaba7064ea7
lightsong <qsfan@qq.com>
2022-09-25 16:01:24 +0800

    package install debug ok: python3 setup.py install

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='FastAPI Skeleton',
-      version='1.0',
+with open('requirements.txt', mode="r", encoding="utf8") as f:
+    required = f.read().splitlines()
+
+setup(
+      name='FastAPI Skeleton',
+      version='1.1',
       description='Sample code for FastAPI',
+      url="https://github.com/fanqingsong/fastapi-ml-skeleton",
+      license='MIT License',
       author='Qingsong Fan',
       author_email='qsfan@qq.com',
-      packages=['fastapi_modules'],
-      )
+      packages=find_packages(),
+      install_requires=required,
+      python_requires=">=3.7",
+)
